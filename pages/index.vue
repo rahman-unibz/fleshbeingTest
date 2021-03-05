8690134d6f58966a82529e6a2b6faa7ae30a72aa
ABM Siddiqur Rahman <rahman.tcd@gmail.com>
2021-03-05 18:09:21 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello possibile future FlashBeing teammate!</h1>
+    <p>This is the page you should edit.</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="postcss" scoped>
+/* Style supports Taylwind. Learn more: https://tailwindcss.com/ */
+</style>
